0f7927a0dcf5e9a19485ed1d73157dddb214b7b1
Pasindu Mallawarachchi <Imalsha_map_e23@engug.ruh.ac.lk>
2024-08-17 15:56:51 +0530

    V-01

--- TemplateDesignpattern/src/Main.java
+++ TemplateDesignpattern/src/Main.java
@@ -3,6 +3,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Step by steps :");
         Coffee coffee = new Coffee();
         Tea tea = new Tea();
 
