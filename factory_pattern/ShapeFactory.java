961f5985a6ca7773df0724698f44af83a5b23e08
Pasindu Mallawarachchi <Imalsha_map_e23@engug.ruh.ac.lk>
2024-10-31 12:32:15 +0530

    Add files via upload

---
+++ factory_pattern/ShapeFactory.java
@@ -0,0 +1,16 @@
+package com.factory_pattern;
+
+public class ShapeFactory {
+    public Shape getShape(String shapeType){
+        if(shapeType == null){
+            return null;
+        }
+        if(shapeType.equalsIgnoreCase("Circle")){
+            return new Circle();
+        }
+        else if(shapeType.equalsIgnoreCase("Rectangle")){
+            return new Rectangle();
+        }
+        return null;
+    }
+}
